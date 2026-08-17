c2e11911780b8bcd7c321b10cfe62929bd22a2c7
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-17 14:08:28 -0300

    feat: criar serviço de JWT

--- src/main/java/com/yurifreitas/spring_security_jwt_api/service/JwtService.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/service/JwtService.java
@@ -1,4 +1,33 @@
 package com.yurifreitas.spring_security_jwt_api.service;
 
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
 public class JwtService {
+
+	private final SecretKey jwtSecretKey;
+
+	@Value("${jwt.expiration}")
+	private long jwtExpiration;
+
+	public String generateToken(Authentication authentication) {
+
+		Instant now = Instant.now();
+
+		return Jwts.builder()
+				.subject(authentication.getName())
+				.issuedAt(Date.from(now))
+				.expiration(Date.from(now.plusSeconds(jwtExpiration)))
+				.signWith(jwtSecretKey)
+				.compact();
+	}
 }
