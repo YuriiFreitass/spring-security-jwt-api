9480e9f3d164c7e80df6640932be185bf5b29d0b
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-17 14:07:35 -0300

    feat: criar controlador de autenticação

--- src/main/java/com/yurifreitas/spring_security_jwt_api/controller/AuthController.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.yurifreitas.spring_security_jwt_api.controller;
 
+import com.yurifreitas.spring_security_jwt_api.dto.LoginRequestDto;
+import com.yurifreitas.spring_security_jwt_api.dto.LoginResponseDto;
+import com.yurifreitas.spring_security_jwt_api.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/login")
+	public LoginResponseDto login(@RequestBody LoginRequestDto request) {
+
+		String token = authService.login(
+				request.username(),
+				request.password()
+		);
+
+		return new LoginResponseDto(token);
+	}
 }
