f468b1b231ae8006baf4bb65a6e4efed500a9838
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-17 13:14:45 -0300

    chore: configura estrutura básica do projeto

---
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/SpringSecurityJwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.yurifreitas.spring_security_jwt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApiApplication.class, args);
+	}
+
+}
