aa673b7850f8e725a53c04137740912b347cbc12
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-17 14:08:17 -0300

    feat: criar serviço de autenticação

--- src/main/java/com/yurifreitas/spring_security_jwt_api/service/AuthService.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/service/AuthService.java
@@ -1,4 +1,28 @@
 package com.yurifreitas.spring_security_jwt_api.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+	private final AuthenticationManager authenticationManager;
+	private final JwtService jwtService;
+
+	public String login(String username, String password) {
+
+		Authentication authentication =
+				authenticationManager.authenticate(
+						new UsernamePasswordAuthenticationToken(
+								username,
+								password
+						)
+				);
+
+		return jwtService.generateToken(authentication);
+	}
 }
