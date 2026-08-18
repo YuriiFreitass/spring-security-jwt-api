5e50551c2fe3baf2a9fd2099cf4af026bdb948ad
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-18 09:37:16 -0300

    feat: Configura autenticação JWT no Spring Security

--- src/main/java/com/yurifreitas/spring_security_jwt_api/config/SecurityConfig.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package com.yurifreitas.spring_security_jwt_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +28,8 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/auth/**").permitAll()
 						.anyRequest().authenticated()
+				)
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
 				)
 				.build();
 	}
