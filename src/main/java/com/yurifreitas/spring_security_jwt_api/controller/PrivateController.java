f9e670599df130aedce047e119fb563c68357906
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-18 09:38:27 -0300

    feat: Adiciona endpoint protegido

--- src/main/java/com/yurifreitas/spring_security_jwt_api/controller/PrivateController.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/controller/PrivateController.java
@@ -1,4 +1,15 @@
 package com.yurifreitas.spring_security_jwt_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class PrivateController {
+
+	@GetMapping("/private")
+	public String privateEndpoint() {
+		return "Acesso autorizado!";
+	}
 }
