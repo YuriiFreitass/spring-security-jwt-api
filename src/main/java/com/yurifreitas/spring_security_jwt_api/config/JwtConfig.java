9fae2e373a6617f1f8968db4b00138b4397fb276
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-18 09:36:41 -0300

    Configura validador JWT

--- src/main/java/com/yurifreitas/spring_security_jwt_api/config/JwtConfig.java
+++ src/main/java/com/yurifreitas/spring_security_jwt_api/config/JwtConfig.java
@@ -1,23 +1,29 @@
 package com.yurifreitas.spring_security_jwt_api.config;
 
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class JwtConfig {
 
+	@Value("${jwt.secret}")
+	private String secret;
+
 	@Bean
-	public SecretKey jwtSecretKey(
-			@Value("${jwt.secret}") String secret
-	) {
-		return new SecretKeySpec(
-				secret.getBytes(StandardCharsets.UTF_8),
-				"HmacSHA256"
-		);
+	public SecretKey secretKey() {
+		return Keys.hmacShaKeyFor(secret.getBytes());
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(SecretKey secretKey) {
+		return NimbusJwtDecoder
+				.withSecretKey(secretKey)
+				.build();
 	}
 }
